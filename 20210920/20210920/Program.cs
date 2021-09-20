d8f14b3ca52fbed6afd78dce09eb01e80b2fd48e
utto.norbert <uttonorbert@gmail.com>
2021-09-20 08:21:16 +0200

    kiírás

---
+++ 20210920/20210920/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20210920
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(32);
+        }
+    }
+}
